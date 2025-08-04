84f40388934e3d7f35c06edab5e47dc491b749fb
Vovas <vovaafon25@gmail.com>
2025-08-04 21:54:07 +0300

    reverted

--- db/models.py
+++ db/models.py
@@ -4,8 +4,6 @@ from django.core.exceptions import ValidationError
 from django.db import models
 from django.db.models import DateTimeField, ForeignKey, IntegerField
 
-import settings
-
 
 class Genre(models.Model):
     name = models.CharField(max_length=255, unique=True)
@@ -65,7 +63,7 @@ class User(AbstractUser):
 class Order(models.Model):
     created_at = DateTimeField(auto_now_add=True)
     user = ForeignKey(
-        settings.AUTH_USER_MODEL,
+        User,
         on_delete=models.CASCADE,
         related_name="orders")
 
